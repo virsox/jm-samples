04be2d518294c94bb1714fd7b3b290e20be96442
paulo.sigrist@gmail.com <paulo.sigrist@gmail.com>
2011-09-04 21:07:22 +0000

    Melhorado o player. Criado um servlet para tocar a musica.

---
+++ musiclib/musiclib-web/src/main/java/br/com/jm/musiclib/web/servlet/PlayerServlet.java
@@ -0,0 +1,63 @@
+package br.com.jm.musiclib.web.servlet;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.jm.musiclib.model.Music;
+import br.com.jm.musiclib.web.Player;
+
+/**
+ * Servlet implementation class PlayerServlet
+ */
+@WebServlet(name = "Player", urlPatterns = { "/Player" })
+public class PlayerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Player player;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public PlayerServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		Music music = player.next();
+
+		response.setContentType("audio/mp3");
+		if (music != null) {
+
+			OutputStream out = response.getOutputStream();
+			BufferedInputStream in = new BufferedInputStream(
+					new FileInputStream(music.getFilePath()));
+			int b;
+			while ((b = in.read()) != -1) {
+				out.write(b);
+			}
+
+			in.close();
+			out.close();
+		}
+		// TODO incrementar quantas vezes a musica foi tocada
+
+	}
+
+}
