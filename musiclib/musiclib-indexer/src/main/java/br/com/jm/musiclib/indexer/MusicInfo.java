482c77baa4e9cb2d89a8f02ae378abf8caad190b
paulo.sigrist@gmail.com <paulo.sigrist@gmail.com>
2011-09-07 22:27:20 +0000

    Atualizado JavaDoc e arrumado pequenos bugs e layout

--- musiclib/musiclib-indexer/src/main/java/br/com/jm/musiclib/indexer/MusicInfo.java
+++ musiclib/musiclib-indexer/src/main/java/br/com/jm/musiclib/indexer/MusicInfo.java
@@ -29,82 +29,19 @@ public class MusicInfo implements Serializable {
 	private List<String> tags;
 	/** Nome do arquivo com as informações extraídas */
 	private String fileName;
-	/** Array de bytes com o binário representando a capa do album */
-	private byte[] cover;
 
+	/**
+	 * Construtor padrão.
+	 */
 	public MusicInfo() {
 		this.tags = new ArrayList<String>();
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getArtist() {
-		return artist;
-	}
-
-	public void setArtist(String artist) {
-		this.artist = artist;
-	}
-
-	public String getAlbum() {
-		return album;
-	}
-
-	public void setAlbum(String album) {
-		this.album = album;
-	}
-
-	public String getTrackNumber() {
-		return trackNumber;
-	}
-
-	public void setTrackNumber(String trackNumber) {
-		this.trackNumber = trackNumber;
-	}
-
-	public List<String> getTags() {
-		return tags;
-	}
-
-	public void setTags(List<String> tags) {
-		this.tags = tags;
-	}
-
-	public String getFileName() {
-		return fileName;
-	}
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-
-	 
-	
-	public byte[] getCover() {
-		return cover;
-	}
-
-	public void setCover(byte[] cover) {
-		this.cover = cover;
-	}
-
 	/**
-	 * Adiciona uma tag 
-	 * @param tag Tag a ser adicionada
+	 * Adiciona uma tag
+	 * 
+	 * @param tag
+	 *            Tag a ser adicionada
 	 * @return true se a tag foi inserida
 	 */
 	public boolean addTag(String tag) {
@@ -112,12 +49,119 @@ public class MusicInfo implements Serializable {
 	}
 
 	/**
-	 * Remove uma tag 
-	 * @param tag Tag a ser removida
+	 * Remove uma tag
+	 * 
+	 * @param tag
+	 *            Tag a ser removida
 	 * @return true se a tag foi removida
 	 */
 	public boolean removeTag(String tag) {
 		return this.tags.remove(tag);
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title
+	 *            the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the artist
+	 */
+	public String getArtist() {
+		return artist;
+	}
+
+	/**
+	 * @param artist
+	 *            the artist to set
+	 */
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	/**
+	 * @return the album
+	 */
+	public String getAlbum() {
+		return album;
+	}
+
+	/**
+	 * @param album
+	 *            the album to set
+	 */
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	/**
+	 * @return the trackNumber
+	 */
+	public String getTrackNumber() {
+		return trackNumber;
+	}
+
+	/**
+	 * @param trackNumber
+	 *            the trackNumber to set
+	 */
+	public void setTrackNumber(String trackNumber) {
+		this.trackNumber = trackNumber;
+	}
+
+	/**
+	 * @return the tags
+	 */
+	public List<String> getTags() {
+		return tags;
+	}
+
+	/**
+	 * @param tags
+	 *            the tags to set
+	 */
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+
+	/**
+	 * @return the fileName
+	 */
+	public String getFileName() {
+		return fileName;
+	}
+
+	/**
+	 * @param fileName
+	 *            the fileName to set
+	 */
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
 	}
 
 }
--- musiclib/musiclib-indexer/src/main/java/br/com/jm/musiclib/indexer/StatusBean.java
+++ musiclib/musiclib-indexer/src/main/java/br/com/jm/musiclib/indexer/StatusBean.java
@@ -5,8 +5,12 @@ import java.io.Serializable;
 import javax.ejb.Singleton;
 import javax.enterprise.event.Observes;
 
-
-
+/**
+ * Bean que guarda a última música encontrada pelo indexador de músicas.
+ * 
+ * @author Paulo Sigrist / Wilson A. Higashino
+ * 
+ */
 @Singleton
 public class StatusBean implements Serializable {
 	/**
@@ -14,7 +18,7 @@ public class StatusBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 6445790885225082845L;
 	/**
-	 * 
+	 * Referência para a última música encontrada
 	 */
 	private MusicInfo currentMusicInfo;
 
@@ -23,43 +27,85 @@ public class StatusBean implements Serializable {
 	 */
 	private boolean completed;
 
+	/**
+	 * Recebe um evento do tipo MusicIndexerEvent disparado pelo MusicIndexer.
+	 * Salva a referência da música encontrada e marca se o precesso foi
+	 * finalizado.
+	 * 
+	 * @param event
+	 * 
+	 * @see MusicIndexer
+	 * @see MusicIndexerImpl
+	 */
 	public void process(@Observes MusicIndexerEvent event) {
 		setCurrentMusicInfo(event.getMusicInfo());
 		setCompleted(event.getCompleted());
 	}
 
+	/**
+	 * @return a música atual. Null se não existe.
+	 */
 	public MusicInfo getCurrentMusicInfo() {
 		return currentMusicInfo;
 	}
 
+	/**
+	 * Altera a música atual
+	 * 
+	 * @param currentMusicInfo
+	 */
 	public void setCurrentMusicInfo(MusicInfo currentMusicInfo) {
 		this.currentMusicInfo = currentMusicInfo;
 	}
 
+	/**
+	 * @return o título da música atual ou vazio se não existe
+	 */
 	public String getTitle() {
 		return currentMusicInfo == null ? "" : currentMusicInfo.getTitle();
 	}
 
+	/**
+	 * @return o artista da música atual ou vazio se não existe
+	 */
 	public String getArtist() {
 		return currentMusicInfo == null ? "" : currentMusicInfo.getArtist();
 	}
 
+	/**
+	 * @return o album da música atual ou vazio se não existe
+	 */
 	public String getAlbum() {
 		return currentMusicInfo == null ? "" : currentMusicInfo.getAlbum();
 	}
 
+	/**
+	 * @return o nome do arquivo da música atual ou vazio se não existe
+	 */
 	public String getFileName() {
 		return currentMusicInfo == null ? "" : currentMusicInfo.getFileName();
 	}
 
+	/**
+	 * @return a faixa da música atual ou vazio se não existe
+	 */
 	public String getTrackNumber() {
-		return currentMusicInfo == null ? "" : currentMusicInfo.getTrackNumber();
+		return currentMusicInfo == null ? "" : currentMusicInfo
+				.getTrackNumber();
 	}
 
+	/**
+	 * @return flag indicando que o processo de indexação foi finalizado.
+	 */
 	public boolean getCompleted() {
 		return completed;
 	}
 
+	/**
+	 * Altera a flag q indica que o processo foi finalizado
+	 * 
+	 * @param completed
+	 */
 	public void setCompleted(Boolean completed) {
 		this.completed = completed;
 	}
