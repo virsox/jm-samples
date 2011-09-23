480c9fd7cd75dd7f6612e7d7a4f71d00a29573b2
virsox@gmail.com <virsox@users.noreply.github.com>
2011-09-23 00:42:10 +0000

    Correções finais para o artigo

--- musiclib/musiclib-indexer/src/main/java/br/com/jm/musiclib/indexer/MusicIndexerEvent.java
+++ musiclib/musiclib-indexer/src/main/java/br/com/jm/musiclib/indexer/MusicIndexerEvent.java
@@ -5,49 +5,49 @@ package br.com.jm.musiclib.indexer;
  * 
  */
 public class MusicIndexerEvent {
-	/**
-	 * Informações sobre o arquivo MP3
-	 */
-	private MusicInfo info;
+  /**
+   * Informações sobre o arquivo MP3
+   */
+  private MusicInfo info;
 
-	/**
-	 * Flag para indicar se o processo já foi completado.
-	 */
-	private Boolean completed;
+  /**
+   * Flag para indicar se o processo já foi completado.
+   */
+  private Boolean completed;
 
-	/**
-	 * Construtor simples.
-	 * 
-	 * @param info
-	 *            Objeto com as informações do arquivo mp3 encontrado
-	 */
-	public MusicIndexerEvent(MusicInfo info) {
-		this(info, false);
-	}
+  /**
+   * Construtor simples.
+   * 
+   * @param info
+   *            Objeto com as informações do arquivo mp3 encontrado
+   */
+  public MusicIndexerEvent(MusicInfo info) {
+    this(info, false);
+  }
 
-	/**
-	 * Construtor completo.
-	 * @param info flag indicando se o processo de indexação foi finalizado.
-	 * @param completed Objeto com as informações do arquivo mp3 encontrado.
-	 */
-	public MusicIndexerEvent(MusicInfo info, Boolean completed) {
-		this.info = info;
-		this.completed = completed;
-	}
+  /**
+   * Construtor completo.
+   * @param info flag indicando se o processo de indexação foi finalizado.
+   * @param completed Objeto com as informações do arquivo mp3 encontrado.
+   */
+  public MusicIndexerEvent(MusicInfo info, Boolean completed) {
+    this.info = info;
+    this.completed = completed;
+  }
 
-	/**
-	 * @return objeto com as informações do arquivo mp3 encontrado
-	 */
-	public MusicInfo getMusicInfo() {
-		return info;
-	}
+  /**
+   * @return objeto com as informações do arquivo mp3 encontrado
+   */
+  public MusicInfo getMusicInfo() {
+    return info;
+  }
 
-	/**
-	 * 
-	 * @return retorna a informação se o processo já foi completado.
-	 */
-	public Boolean getCompleted() {
-		return completed;
-	}
+  /**
+   * 
+   * @return retorna a informação se o processo já foi completado.
+   */
+  public Boolean getCompleted() {
+    return completed;
+  }
 
 }
