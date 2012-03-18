5f36d3f88782003436bbd0aec6bd324e830394ac
paulo.sigrist@gmail.com <paulo.sigrist@gmail.com@deb4da82-55cb-1a4c-2d66-2846cfe9a5ee>
2012-03-18 18:21:47 +0000

    Atualizado app e documentação

--- trunk/cv-searcher/searcher-simple-web/src/main/java/br/com/jm/cvsearcher/service/SearchService.java
+++ trunk/cv-searcher/searcher-simple-web/src/main/java/br/com/jm/cvsearcher/service/SearchService.java
@@ -1,5 +1,6 @@
 package br.com.jm.cvsearcher.service;
 
+import java.io.File;
 import java.util.List;
 
 import br.com.jm.cvsearcher.model.Curriculum;
@@ -14,13 +15,14 @@ import br.com.jm.cvsearcher.model.Curriculum;
 public interface SearchService {
 
 	/**
-	 * Adiciona um novo currículo.
+	 * Adiciona e indexa um novo currículo.
 	 * 
 	 * @param cv
 	 *            Currículo a ser adicionado
-	 * @return true se o currículo foi adicionado com sucesso.
+	 * 
+	 * @throws CurriculumException
 	 */
-	public boolean addCurriculum(Curriculum cv);
+	public void addCurriculum(Curriculum cv) throws CurriculumException;
 
 	/**
 	 * Efetua a busca de currículos baseado no nome do candidato.
@@ -28,8 +30,10 @@ public interface SearchService {
 	 * @param name
 	 *            Parte do nome do candidato
 	 * @return Lista de resultados da busca
+	 * @throws CurriculumException
 	 */
-	public List<Curriculum> findCVByName(String name);
+	public List<Curriculum> findCVByName(String name)
+			throws CurriculumException;
 
 	/**
 	 * Efetua a busca de currículos baseado no conteúdo do currículo.
@@ -37,21 +41,28 @@ public interface SearchService {
 	 * @param content
 	 *            Palavra chave a ser buscado no contaúdo do currículo
 	 * @return Lista de resultados da busca
+	 * @throws CurriculumException
 	 */
-	public List<Curriculum> findCVByContent(String content);
+	public List<Curriculum> findCVByContent(String content)
+			throws CurriculumException;
 
 	/**
 	 * Indexa um currículo específico.
 	 * 
 	 * @param cv
 	 *            Currículo a ser indexado
+	 * @param file
+	 *            Caminho do arquivo que está sendo indexado
+	 * @throws CurriculumException
 	 */
-	public void index(Curriculum cv);
+	public void index(Curriculum cv, File file) throws CurriculumException;
 
 	/**
-	 * Indexa todos os currículos existentes.
+	 * Reindexa todos os currículos existentes.
 	 * 
 	 * O índice existente é completamente substituído por um novo.
+	 * 
+	 * @throws CurriculumException
 	 */
-	public void indexAll();
+	public void indexAll() throws CurriculumException;
 }
--- trunk/cv-searcher/searcher-simple-web/src/main/java/br/com/jm/cvsearcher/web/SearchBean.java
+++ trunk/cv-searcher/searcher-simple-web/src/main/java/br/com/jm/cvsearcher/web/SearchBean.java
@@ -4,14 +4,23 @@ import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
 
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.jm.cvsearcher.model.Curriculum;
+import br.com.jm.cvsearcher.service.CurriculumException;
 import br.com.jm.cvsearcher.service.SearchService;
 
-@SessionScoped
+/**
+ * Bean CDI para busca de currículos.
+ * 
+ * 
+ * 
+ */
+@RequestScoped
 @Named
 public class SearchBean implements Serializable {
 	/**
@@ -19,36 +28,76 @@ public class SearchBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 7942216607824066013L;
 
+	/**
+	 * Enum para indicar qual é o tipo de busca a ser realizado.
+	 * 
+	 * @author Paulo Sigrist / Wilson A. Higashino
+	 * 
+	 */
 	public enum SearchType {
-		NAME, CONTENT
+		/** Buscas por nome */
+		NAME,
+		/** Buscas pelo conteúdo do currículo */
+		CONTENT
 	}
 
+	/** Palavra chave a ser buscada */
 	private String keyword;
+	/** Tipo de busca a ser feita */
 	private SearchType type;
+	/** Resultado da busca */
 	private List<Curriculum> results;
 
+	/**
+	 * Construtor padrão. Inicializa os atributos <tt>result</tt> como uma lista
+	 * vazia e o campo <tt>type</tt> como o tipo {@link SearchType#CONTENT}.
+	 */
 	public SearchBean() {
 		this.results = Collections.emptyList();
-		this.type = SearchType.NAME;
+		this.type = SearchType.CONTENT;
 	}
 
+	/** Bean de serviço. Injetado pelo container. */
 	@Inject
 	private SearchService service;
 
+	/**
+	 * Efetua a busca.
+	 * 
+	 * Se o campo <tt>type</tt> for inválido, retorna uma lista vazia.
+	 * 
+	 * @see SearchService#findCVByContent(String)
+	 * @see SearchService#findCVByName(String)
+	 */
 	public void search() {
-
+		FacesContext context = FacesContext.getCurrentInstance();
+		
+		// Verifica o tipo de busca a ser feito
 		switch (type) {
 		case NAME: {
-			results = service.findCVByName(keyword);
+			// Caso seja por nome, chama o método respectivo do seriço de busca
+			try {
+				results = service.findCVByName(keyword);
+			} catch (CurriculumException e) {
+				context.addMessage(null, new FacesMessage(
+						FacesMessage.SEVERITY_ERROR, "Erro", e.getMessage()));
+			}
 		}
 			;
 			break;
 		case CONTENT: {
-			results = service.findCVByContent(keyword);
+			// Caso seja por conteúdo, chama o método respectivo do seriço de busca
+			try {
+				results = service.findCVByContent(keyword);
+			} catch (CurriculumException e) {
+				context.addMessage(null, new FacesMessage(
+						FacesMessage.SEVERITY_ERROR, "Erro", e.getMessage()));
+			}
 		}
 			;
 			break;
 		default: {
+			// Caso seja por inválido, retorne uma lista vazia
 			results = Collections.emptyList();
 		}
 			;
@@ -57,22 +106,39 @@ public class SearchBean implements Serializable {
 
 	}
 
+	/**
+	 * @return the keyword
+	 */
 	public String getKeyword() {
 		return keyword;
 	}
 
+	/**
+	 * @param keyword
+	 *            the keyword to set
+	 */
 	public void setKeyword(String keyword) {
 		this.keyword = keyword;
 	}
 
+	/**
+	 * @return the type
+	 */
 	public SearchType getType() {
 		return type;
 	}
 
+	/**
+	 * @param type
+	 *            the type to set
+	 */
 	public void setType(SearchType type) {
 		this.type = type;
 	}
 
+	/**
+	 * @return the results
+	 */
 	public List<Curriculum> getResults() {
 		return results;
 	}
