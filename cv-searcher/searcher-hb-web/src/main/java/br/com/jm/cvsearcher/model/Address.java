90e9027e8cd3f2444eab5d13f45ad50b7e5e0dc7
virsox@gmail.com <virsox@users.noreply.github.com>
2012-08-02 02:06:07 +0000

    Adição de classe Address

---
+++ cv-searcher/searcher-hb-web/src/main/java/br/com/jm/cvsearcher/model/Address.java
@@ -0,0 +1,67 @@
+package br.com.jm.cvsearcher.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+  private Long id;
+  private String streetName;
+  private Integer number;
+  private String city;
+  private String state;
+  
+  public Address() { }
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+  
+  @Column
+  public String getStreetName() {
+    return streetName;
+  }
+
+  public void setStreetName(String streetName) {
+    this.streetName = streetName;
+  }
+
+  @Column
+  public Integer getNumber() {
+    return number;
+  }
+
+  public void setNumber(Integer number) {
+    this.number = number;
+  }
+
+  @Column
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  @Column
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+  
+  
+  
+}
--- cv-searcher/searcher-hb-web/src/main/java/br/com/jm/cvsearcher/model/Curriculum.java
+++ cv-searcher/searcher-hb-web/src/main/java/br/com/jm/cvsearcher/model/Curriculum.java
@@ -1,16 +1,20 @@
 package br.com.jm.cvsearcher.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 import org.hibernate.search.annotations.Analyze;
 import org.hibernate.search.annotations.DocumentId;
 import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Index;
 import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.IndexedEmbedded;
 import org.hibernate.search.annotations.Store;
 
 /**
@@ -30,6 +34,8 @@ public class Curriculum {
   private String name;
   /** Email do candidato */
   private String email;
+  /** Endereço do candidato. */
+  private Address address = new Address();
   /** Conteúdo do currículo do candidato */
   private String content;
 
@@ -110,6 +116,17 @@ public class Curriculum {
     this.email = email;
   }
 
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "fk_address_id")
+  @IndexedEmbedded
+  public Address getAddress() {
+    return this.address;
+  }
+  
+  public void setAddress(Address address) {
+    this.address = address;
+  }
+  
   /**
    * @return the content
    */
